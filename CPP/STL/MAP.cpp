108a2dd008eb677c86878106ec0a6c3206a37928
U-Hp\Bottomboy <Bottomboy@Hp.(none)>
2019-12-22 16:52:40 +0530

    Bridge Pattern..

--- CPP/STL/MAP.cpp
+++ CPP/STL/MAP.cpp
@@ -12,19 +12,34 @@ int main()
 	ageName.insert ( pair <int, char *> (3, "Bcdefg" ) );
 	ageName.insert ( pair <int, char *> (2, "Cdefgh" ) );
 	ageName.insert ( pair <int, char *> (4, "Defghi" ) );
+    ageName[10] = "Efghij";
 
 	map <int, char *>::iterator it;
 
 	for ( it = ageName.begin(); it != ageName.end(); it++ )
 	{
-		cout << "    " << it->first << "   " << it->second;
+		cout << "    " << it->first << "   " << it->second << endl;
 	}
 	
-	map<int, char *> nameAge;
+	map<char*, int> nameAge;
 
 	nameAge.insert ( pair <char *, int> ("Abcdef", 1 ));
-	nameAge.insert ( pair <char *, int> (2, "Bcdefg" ));
+	nameAge.insert ( pair <char *, int> ("2", 9 ));
 
+	for ( map<char*, int>::iterator it2 = nameAge.begin(); it2 != nameAge.end(); it2++ )
+	{
+		cout << "    " << it2->first << "   " << it2->second << endl;
+	}
+    map<char*, int>::iterator findItr = nameAge.find ("Abcdef");
 
+    if ( findItr != nameAge.end() )
+        cout << "find (Abcdef) = " << findItr->first << " , " << findItr->second << endl;
+    else
+        cout << "Not found" ;
+            
+
+//    cout << "find (bcdef) = " << nameAge.find ("bcdef") << endl;
+
+    return 0;
 }
 
